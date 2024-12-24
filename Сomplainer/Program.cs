16f1f243d4e43b3ace000ce1fc22b564d9113d0d
Sergey <ushenkoserega@yandex.ru>
2024-12-24 12:30:49 +0600

    Initial

---
+++ Сomplainer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сomplainer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Complainer main service is running...");
+            Console.WriteLine("Press ENTER key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
